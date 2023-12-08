d0866598437fdbaf75884ac7bfce422b47d7db5d
LOliviaL <liuhaoyue1024@gmail.com>
2023-12-08 07:17:01 +0100

    page404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {Page404Component} from "./page404/page404.component";
+import {HomepageComponent} from "./homepage/homepage.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'HomepageComponent', pathMatch: 'full' },
+  { path:"/home", component: HomepageComponent},
+  { path: "**", component: Page404Component},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Page404Component } from './page404/page404.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page404Component,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
