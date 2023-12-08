6bd6ba5f223788c8f49a8a3129b4caf1c60b4c2f
emma <chen.emma1001@gmail.com>
2023-12-08 07:36:25 +0100

    final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './component/homepage/homepage.component';
+import { Page404Component } from './component/page404/page404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'HomePageComponent', pathMatch: 'full' },
+  { path: 'homepage', component: HomepageComponent },
+  { path: '**', component:Page404Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HomepageComponent } from './component/homepage/homepage.component';
+import { Page404Component } from './component/page404/page404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomepageComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
